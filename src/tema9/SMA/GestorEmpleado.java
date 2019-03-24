82ed3cf758371ed6ebe51a15602889d4f053ddcb
pietrodeocre <pietrodeocre@pietrodeocre-desktop>
2019-03-24 10:09:53 +0100

    cambio2

---
+++ src/tema9/SMA/GestorEmpleado.java
@@ -0,0 +1,17 @@
+package tema9.SMA;
+
+import Empleado.*;
+
+public class GestorEmpleado {
+	public static void main(String[] args) {
+		Empleado empleado = {new Empleado("569587A","Javier","Gómez",1225.00),
+	}
+	
+	public static void sueldoMayor(Empleado empleados[]){
+		
+	}
+	
+	public static void mostrarTodos(Empleado empleados[]){
+		
+	}
+}
