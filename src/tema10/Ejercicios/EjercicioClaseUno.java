748a61843f9894ec84bf582c5b6f6c83a274ecb1
PietrodeOcre <pietrodeocre@gmail.com>
2019-04-23 21:56:18 +0200

    cambios2 23-04-19

---
+++ src/tema10/Ejercicios/EjercicioClaseUno.java
@@ -0,0 +1,41 @@
+package tema10.Ejercicios;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class EjercicioClaseUno {
+	
+	public static void main(String[] args) {
+		
+		List<String> listado = new ArrayList<String>();
+		
+		listado.add("Hola");
+		listado.add(" el ");
+		listado.add(" manu ");
+		listado.add(" es ");
+		listado.add(" Gay!!");
+		
+		/*
+		int cont = 0;
+		for (String string : listado) {
+			listado.set(cont++, string.toUpperCase());
+			System.out.println(string);
+		}
+		*/
+		
+		/*
+		int cont = 0;
+		Iterator<String> iterator = listado.iterator();
+		while (iterator.hasNext()) {
+			String itera = iterator.next();
+			listado.set(cont++,itera.toUpperCase());
+			System.out.println(itera);
+		}
+		*/
+		
+		System.out.println(listado);
+	}
+	
+}
