4a7cf035826e605dcfe2966369b1d97e97436dab
pietrodeocre <pietrodeocre@pietrodeocre-desktop>
2019-05-01 15:29:06 +0200

    cambios2 01-05-19

--- src/tema10/HojaEjercicios/Once.java
+++ src/tema10/HojaEjercicios/Once.java
@@ -10,9 +10,11 @@ public class Once {
 devuelva un nuevo Map<String,Integer> que sea el invertido del original. Si hay más de
 una clave en el Map original que tiene el mismo valor, el valor de la clave del Map que
 se devuelve será el último valor iterado del Map. Por ejemplo, si el método recibe
-como parámetro el siguiente Map: {42=Marty, 81=Sue, 17=Ed, 31=Dave, 56=Ed,
-3=Marty, 29=Ed}, el método debe retornar el siguiente Map {Marty=3, Sue=81, Ed=29,
-Dave=31}. El prototipo de la función es el siguiente:
+como parámetro el siguiente Map: 
+{42=Marty, 81=Sue, 17=Ed, 31=Dave, 56=Ed, 3=Marty, 29=Ed}, el método debe retornar el 
+siguiente Map 
+{Marty=3, Sue=81, Ed=29, Dave=31}
+. El prototipo de la función es el siguiente:
 public static Map<String,Integer> reverse(Map<Integer, String> mapStr)
 	 */
 	
@@ -20,12 +22,26 @@ public static Map<String,Integer> reverse(Map<Integer, String> mapStr)
 		
 		Map<Integer, String> lista = new HashMap<>();
 		
+		lista.put(42, "Marty");
+		lista.put(81, "Sue");
+		lista.put(17, "Ed");
+		lista.put(31, "Dave");
+		lista.put(56, "Ed");
+		lista.put(3, "Marty");
+		lista.put(29, "Ed");
+		
+		System.out.println(lista);
+		//reverse(lista);
+		
 	}
 	
 	public static Map<String,Integer> reverse(Map<Integer, String> mapStr){
 		
+		//Collections.reverse(); 
 		
 		
+		
+		System.out.println(mapStr);
 		
 		return null;
 
