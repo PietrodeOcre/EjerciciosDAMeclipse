dbdf1f4f10f17400771eddca5979b662cf5e67ba
PietrodeOcre <pietrodeocre@gmail.com>
2019-05-10 12:49:03 +0200

    cambios1 10-05-19

---
+++ src/tema10/HojaEjercicios/II/OrdenaPorCalleYNumero.java
@@ -0,0 +1,14 @@
+package tema10.HojaEjercicios.II;
+
+import java.util.Comparator;
+
+public class OrdenaPorCalleYNumero implements Comparator<Direccion>{
+	
+
+	@Override
+	public int compare(Direccion o1, Direccion o2) {
+		// TODO Auto-generated method stub
+		return o1.compareTo(o2);
+	}
+
+}
