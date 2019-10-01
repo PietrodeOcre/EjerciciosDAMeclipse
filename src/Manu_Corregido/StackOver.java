169daf1a825315d9f72afec505c46b499742386c
PietrodeOcre <pietrodeocre@gmail.com>
2019-10-01 11:07:03 +0200

    cambios1 01-10-19

---
+++ src/Manu_Corregido/StackOver.java
@@ -0,0 +1,20 @@
+package Manu_Corregido;
+
+public class StackOver {
+	
+	public static void main(String[] name) {
+	    int x = 300;
+	    int y = 200;
+	    int z= 0;
+	    int w = x + y + z;
+	    //System.out.print(w);
+	    
+	    String dni =  "11111111-N";
+	    
+	    int poscion = dni.length();
+	    
+	    System.out.println(dni.substring(poscion-1));
+	    
+	}
+	
+}
